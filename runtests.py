b68b88c358bf8d1d44453a9208af15bf1f5ae117
0dminnimda <0dminnimda@gmail.com>
2021-07-16 19:14:35 +0300

    runtests: format

--- runtests.py
+++ runtests.py
@@ -307,8 +307,8 @@ def update_cpp11_extension(ext):
     if clang_version:
         ext.extra_compile_args.append("-std=c++11")
         if sys.platform == "darwin":
-          ext.extra_compile_args.append("-stdlib=libc++")
-          ext.extra_compile_args.append("-mmacosx-version-min=10.7")
+            ext.extra_compile_args.append("-stdlib=libc++")
+            ext.extra_compile_args.append("-mmacosx-version-min=10.7")
         return ext
 
     return EXCLUDE_EXT
