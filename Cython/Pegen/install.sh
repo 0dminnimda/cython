681c36d521de72f07197171c2f471c3f370b153d
0dminnimda <0dminnimda@gmail.com>
2022-06-28 13:54:31 +0300

    Create Pegen\install.sh

---
+++ Cython/Pegen/install.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+
+# python -m pip install -U pip setuptools wheel || exit 1
+
+echo "Installing virtualenv"
+python -m pip install virtualenv || exit 1
+
+echo "Making virtualenv"
+python -m virtualenv Cython/Pegen/genenv || exit 1
+
+echo "Activating virtualenv"
+. Cython/Pegen/genenv/bin/activate || exit 1
+
+echo "Installing pegen"
+python -m pip install git+https://github.com/0dminnimda/pegen.git@cython_peg || exit 1
+
+echo "Deactivating virtualenv"
+deactivate || exit 1
