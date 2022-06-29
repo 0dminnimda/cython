0aa6858ca3d7a0b7a131d9e4a829dfbe3bdde452
0dminnimda <0dminnimda@gmail.com>
2022-06-29 03:54:58 +0300

    Parser: move parsing function to init and fix the parser

--- Cython/Parser/__init__.py
+++ Cython/Parser/__init__.py
@@ -1,1 +1,19 @@
-# empty file
+from pegen.tokenizer import Tokenizer
+
+from .Parser import CythonParser
+from .TokenStream import TokenStreamer
+
+
+def parse(f, context, source_desc, scope, pxd, full_module_name):
+    token_stream = TokenStreamer(f, source_desc, source_encoding=f.encoding,
+                                 scope=scope, context=context)
+    verbose = context.options.verbose
+    tokenizer = Tokenizer(iter(token_stream), verbose=verbose)  # , path=path)
+    parser = CythonParser(
+        tokenizer,
+        source_desc,
+        verbose=verbose,
+        filename=full_module_name,
+        py_version=None,
+    )
+    return parser.parse("file")
